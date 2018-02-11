23a57e58c5ec34b87812162351f4f23ef0c03b4d
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-11 15:42:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function play(){
        var audio = document.getElementById("audio");
        audio.play();
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 49) {
+        audio.play();
+    }
+}, true);
