f66a329c2e1fc6911208eb73e1bb73e0fa945141
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-02-11 16:36:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 function playmusic_1(){
-       var audio = document.getElementById("audio");
+       var audio_1 = document.getElementById("audio_1");
        audio.play();
 }
 
 function playmusic_2(){
-       var audio = document.getElementById("audio");
+       var audio_2 = document.getElementById("audio_2");
        audio.play();
 }
 
 function playmusic_3(){
-       var audio = document.getElementById("audio");
+       var audio_3 = document.getElementById("audio_3");
        audio.play();
 }
 
 document.addEventListener('keydown', function(event) {
     if (event.keyCode == 49) {
-        audio.play();
+        audio_1.play();
+    }else if(event.keyCode == 50){
+    	audio_2.play();
+    }else if(event.keyCode == 51){
+    	audio_3.play();
     }
 }, true);
